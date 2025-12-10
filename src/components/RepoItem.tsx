86f77ce6ad304c71522a6ae138361d4401ded6c9
laboratorio1.fs <arely.pazmino@uisek.edu.ec>
2025-12-10 10:34:28 -0500

    Lab 8 Finalizado

--- src/components/RepoItem.tsx
+++ src/components/RepoItem.tsx
@@ -1,18 +1,19 @@
 import { IonItem, IonLabel, IonThumbnail } from '@ionic/react';
 import './RepoItem.css';
+import { RepositoryItem } from '../interfaces/RepositoryItem';
 
-interface RepoProps {
-  name: string;
-  imageUrl: string;
-}
-
-const RepoItem: React.FC<RepoProps> = ({ name, imageUrl }) => {
+const RepoItem: React.FC<{ repo: RepositoryItem}> = ({ repo }) => {
   return (
    <IonItem>
         <IonThumbnail slot='start'>
-            <img src={imageUrl} alt={name} />
+            <img src={repo.imageUrl || "https://i.pinimg.com/736x/dc/f8/06/dcf8065bc47347d068bf8a8a93747c36.jpg"} alt={repo.name} />
         </IonThumbnail>
-        <IonLabel>{name}</IonLabel>
+        <IonLabel>
+        <h2>{repo.name}</h2>
+        <p> {repo.description}</p>
+        <p> Propietario: {repo.owner}</p>
+        <p> Lenguaje: {repo.language}</p>
+        </IonLabel>
    </IonItem>
   );
 };
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,34 +1,44 @@
-import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar, useIonViewDidEnter } from '@ionic/react';
+import { useState } from 'react';
 import './Tab1.css';
 import RepoItem from '../components/RepoItem';
+import { RepositoryItem } from '../interfaces/RepositoryItem';
+import { fetchRepositories } from '../services/GithubService';
 
 const Tab1: React.FC = () => {
+  const [repos, setRepos] = useState<RepositoryItem[]>([]);
+
+  const loadRepos = async () => {
+    const reposData = await fetchRepositories ();
+    setRepos(reposData);
+  };
+
+  useIonViewDidEnter(() => {
+    console.log("IonViewDidEnter - Cargando repositorios");
+    loadRepos();
+  });
+
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Repositorios</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Repositorios</IonTitle>
           </IonToolbar>
         </IonHeader>
        <IonList>
-        <RepoItem
-          name= "android-project"
-          imageUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Android_robot.svg/1022px-Android_robot.svg.png'
-          />
-           <RepoItem
-          name= "ios-project"
-          imageUrl='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj1vbc2LV9BPqahmLu4I7qv47nsXNJiWkDOA&s'
-          />
-           <RepoItem
-          name= "ionic-project"
-          imageUrl='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBOyl2On-wIoskoQa3oH5-TpFjZQ-2FaVDHA&s'
-          />
+        {repos.map((repo, index) => (
+          <RepoItem
+            key={index}
+            repo={repo}
+            />
+        ))}
        </IonList>
         
       </IonContent>
